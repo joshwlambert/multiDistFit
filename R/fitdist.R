752001e0f6baea9ada3fa03d2b8e9b5cb35958cb
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-01-11 17:37:19 +0000

    added functions to compare model output from coarseDataTools

--- R/fitdist.R
+++ R/fitdist.R
@@ -17,12 +17,17 @@ multi_fitdist <- function(data, models) {
 
   # fit distributions to data
   if (is.data.frame(data)) {
-    res <- lapply(
-      models,
-      fitdistrplus::fitdistcens,
-      censdata = data
-    )
-
+    if (ncol(data) == 2) {
+      res <- lapply(
+        models,
+        fitdistrplus::fitdistcens,
+        censdata = data
+      )
+    } else {
+      res <- fit_cdt_dist(data = data, models = models)
+      # data is formatted in fit_cdt_dist so return early
+      return(res)
+    }
   } else {
     res <- lapply(
       models,
@@ -79,4 +84,105 @@ goodness_of_fit <- function(data, models) {
 
   # return the goodness-of-fit statistics for each model
   fitdistrplus::gofstat(fitdist, fitnames = models)
+}
+
+#' Fits probability distributions using `coarseDataTools::dic.fit()`
+#'
+#' @inheritParams multi_fitdist
+#'
+#' @return A data frame of all models
+#' @export
+#'
+#' @examples
+#' library(coarseDataTools)
+#' data("nycH1N1")
+#' fit_cdt_dist(data = nycH1N1, models = c("lnorm", "weibull"))
+fit_cdt_dist <- function(data, models) {
+
+  # convert models to format accepted by dic.fit()
+  models <- gsub(pattern = "lnorm", replacement = "L", x = models)
+  models <- gsub(pattern = "gamma", replacement = "G", x = models)
+  models <- gsub(pattern = "weibull", replacement = "W", x = models)
+
+  fitdist <- lapply(models, function(x, data) {
+    if (x == "G") {
+      # gamma distribution fitting requires bootstrapping
+      coarseDataTools::dic.fit(dat = data, dist = x, n.boots = 100)
+    } else {
+      coarseDataTools::dic.fit(dat = data, dist = x)
+    }
+  }, data)
+
+  # extract loglikelihood
+  loglik <- sapply(fitdist, "slot", "loglik")
+
+  # calculate aic and bic
+  aic <- calc_aic(loglik)
+  bic <- calc_bic(loglik, data)
+
+  # change models back to original
+  models <- gsub(pattern = "^L$", replacement = "lnorm", x = models)
+  models <- gsub(pattern = "^G$", replacement = "gamma", x = models)
+  models <- gsub(pattern = "^W$", replacement = "weibull", x = models)
+
+  # package results into data frame
+  res <- data.frame(models = models, loglik = loglik, aic = aic, bic = bic)
+
+  # order the results from best fit to worst
+  res <- res[order(res$aic, method = "radix"), ]
+  rownames(res) <- NULL
+
+  # return results
+  res
+}
+
+#' Calculates the Akaike information criterion for the loglikelihood of a two
+#' parameter probability distribution
+#'
+#' @param loglik A vector or single number the loglikelihood of the model
+#'
+#' @return A single or vector of numerics equal to the input vector length
+#' @export
+#'
+#' @examples
+#' calc_aic(loglik = -110)
+calc_aic <- function(loglik) {
+
+  # make loglik a logLik class for AIC method
+  class(loglik) <- "logLik"
+
+  # set degrees of freedom (TODO: allow df to change)
+  attr(loglik, "df") <- 2
+
+  # calculate and return AIC
+  stats::AIC(loglik)
+}
+
+#' Calculates the Bayesian information criterion for the loglikelihood of a two
+#' parameter probability distribution
+#'
+#' @inheritParams calc_aic
+#' @inheritParams multi_fitdist
+#'
+#' @return A single or vector of numerics equal to the input vector length
+#' @export
+#'
+#' @examples
+#' library(coarseDataTools)
+#' data("nycH1N1")
+#' calc_bic(loglik = -110, data = nycH1N1)
+calc_bic <- function(loglik, data) {
+
+  # make loglik a logLik class for BIC method
+  class(loglik) <- "logLik"
+
+  # set degrees of freedom (TODO: allow df to change)
+  attr(loglik, "df") <- 2
+
+  # set number of observations
+  attr(loglik, "nobs") <- nrow(data)
+
+  # calculate and return BIC
+  stats::BIC(loglik)
+
 }
